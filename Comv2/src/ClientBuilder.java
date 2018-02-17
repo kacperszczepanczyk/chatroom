12f4e779607c7fa2fd80a8c8beec1e38b602ed24
Kacper Szczepańczyk <kacper.szczepanczyk@gmail.com>
2018-02-17 14:06:38 +0100

    c

--- Comv2/src/ClientBuilder.java
+++ Comv2/src/ClientBuilder.java
@@ -1,7 +1,7 @@
 public class ClientBuilder {
 
 	public static void main(String[] args) {
-		Client client = new Client("83.20.213.203", 5000);
+		Client client = new Client("192.168.0.4", 5000);
 		client.createInputOutputToServer();
 		client.initializeGUI();
 		client.initializeClientListener();
